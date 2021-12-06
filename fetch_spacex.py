6ee7ad224a1845c36eb6230d3106879957f06a51
mogubudu <pipkin.vladislav@gmail.com>
2021-12-06 23:33:17 +0300

    Deleted functions for work with files

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,45 +1,5 @@
-import os
 import requests
-
-from urllib.parse import urlparse, unquote
-
-
-def get_file_extencion(url):
-    path = urlparse(url)[2]
-    path = unquote(path)
-    extencion = os.path.splitext(path)[1]
-    return extencion
-
-
-def get_filename(url):
-    path = urlparse(url)[2]
-    path = unquote(path)
-    filename = os.path.split(path)[1]
-    filename = os.path.splitext(filename)[0]
-    filename = filename.replace(' ', '_')
-    return filename
-
-
-def create_filename(url):
-    filename = get_filename(url)
-    extencion = get_file_extencion(url)
-    return f'{filename}{extencion}'
-
-
-def download_image(image_name, image_url, path_to_save='images/'):
-    directory = path_to_save
-    image_name = image_name
-    image_url = image_url
-
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    responce = requests.get(image_url)
-    responce.raise_for_status()
-
-    with open(f'{directory}{image_name}', 'wb') as file:
-        file.write(responce.content)
-
+from file_handler import download_image, create_filename
 
 def fetch_spacex_last_launch():
     spacex_api_url = 'https://api.spacexdata.com/v4/launches/'
